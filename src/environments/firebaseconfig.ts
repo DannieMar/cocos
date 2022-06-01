c2a286d80c5cff251e5ffc5bf5ff3bf767732771
Dannie Marom <danniemarom@Dannies-MacBook-Pro.local>
2022-05-31 23:36:17 -0500

    logic and structure p_290282

---
+++ src/environments/firebaseconfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDyg33fvCas5vQRDRJTIq9N73Pxy0PIgnw",
+  authDomain: "contactosapp-290282.firebaseapp.com",
+  projectId: "contactosapp-290282",
+  storageBucket: "contactosapp-290282.appspot.com",
+  messagingSenderId: "660123578850",
+  appId: "1:660123578850:web:d4b82cc4dadaa3a7cbc9bb",
+  measurementId: "G-SWYX7W7NCV"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
